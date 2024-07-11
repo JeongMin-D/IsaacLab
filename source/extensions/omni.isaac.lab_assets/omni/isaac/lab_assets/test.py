7be94933546f05710c0ac5bf91bf9cadc3d693ef
jmin <j25ng.py@gmail.com>
2024-07-11 10:27:59 +0900

    custom

---
+++ source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/test.py
@@ -0,0 +1,298 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration for the ANYbotics robots.
+
+The following configuration parameters are available:
+
+* :obj:`ANYMAL_B_CFG`: The ANYmal-B robot with ANYdrives 3.0
+* :obj:`ANYMAL_C_CFG`: The ANYmal-C robot with ANYdrives 3.0
+* :obj:`ANYMAL_D_CFG`: The ANYmal-D robot with ANYdrives 3.0
+
+Reference:
+
+* https://github.com/ANYbotics/anymal_b_simple_description
+* https://github.com/ANYbotics/anymal_c_simple_description
+* https://github.com/ANYbotics/anymal_d_simple_description
+
+"""
+
+import omni.isaac.lab.sim as sim_utils
+from omni.isaac.lab.actuators import ActuatorNetLSTMCfg, DCMotorCfg, IdealPDActuatorCfg
+from omni.isaac.lab.assets.articulation import ArticulationCfg
+from omni.isaac.lab.utils.assets import ISAACLAB_NUCLEUS_DIR
+
+##
+# Configuration - Actuators.
+##
+
+ANYDRIVE_3_SIMPLE_ACTUATOR_CFG = DCMotorCfg(
+    joint_names_expr=[".*HAA", ".*HFE", ".*KFE"],
+    saturation_effort=120.0,
+    effort_limit=80.0,
+    velocity_limit=7.5,
+    stiffness={".*": 40.0},
+    damping={".*": 5.0},
+)
+"""Configuration for ANYdrive 3.x with DC actuator model."""
+
+
+ANYDRIVE_3_LSTM_ACTUATOR_CFG = ActuatorNetLSTMCfg(
+    joint_names_expr=[".*HAA", ".*HFE", ".*KFE"],
+    network_file=f"{ISAACLAB_NUCLEUS_DIR}/ActuatorNets/ANYbotics/anydrive_3_lstm_jit.pt",
+    saturation_effort=120.0,
+    effort_limit=80.0,
+    velocity_limit=7.5,
+)
+"""Configuration for ANYdrive 3.0 (used on ANYmal-C) with LSTM actuator model."""
+
+
+##
+# Configuration - Articulation.
+##
+
+ANYMAL_B_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-B/anymal_b.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.02, rest_offset=0.0),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.6),
+        joint_pos={
+            ".*HAA": 0.0,  # all HAA
+            ".*F_HFE": 0.4,  # both front HFE
+            ".*H_HFE": -0.4,  # both hind HFE
+            ".*F_KFE": -0.8,  # both front KFE
+            ".*H_KFE": 0.8,  # both hind KFE
+        },
+    ),
+    actuators={"legs": ANYDRIVE_3_LSTM_ACTUATOR_CFG},
+    soft_joint_pos_limit_factor=0.95,
+)
+"""Configuration of ANYmal-B robot using actuator-net."""
+
+
+# HAMSTER_N_CFG = ArticulationCfg(
+#     spawn=sim_utils.UsdFileCfg(
+#         usd_path=f"/home/jmin/isaac_ws/isaaclab_test/hamster_n.usd",
+#         activate_contact_sensors=True,
+#         rigid_props=sim_utils.RigidBodyPropertiesCfg(
+#             disable_gravity=False,
+#             retain_accelerations=False,
+#             linear_damping=0.0,
+#             angular_damping=0.0,
+#             max_linear_velocity=1000.0,
+#             max_angular_velocity=1000.0,
+#             max_depenetration_velocity=1.0,
+#         ),
+#         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+#             enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+#         ),
+#         # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.02, rest_offset=0.0),
+#     ),
+#     init_state=ArticulationCfg.InitialStateCfg(
+#         pos=(0.0, 0.0, 0.6),
+#         joint_pos={
+#             "LF_HAA": 0.08,  # all HAA
+#             "LH_HAA": 0.08,  # all HAA
+#             "RF_HAA": -0.08,  # all HAA
+#             "RH_HAA": -0.08,  # all HAA
+#             "LF_HFE": 0.1332,  # both front HFE
+#             "LH_HFE": 0.1332,  # both front HFE
+#             "RF_HFE": -0.1332,  # both hind HFE
+#             "RH_HFE": -0.1332,  # both hind HFE
+#             ".*_KFE": 0.0,  # both front KFE
+#         },
+#     ),
+#     actuators={"legs": ANYDRIVE_3_LSTM_ACTUATOR_CFG},
+#     soft_joint_pos_limit_factor=0.47,
+# )
+# """Configuration of ANYmal-C robot using actuator-net."""
+
+
+HAMSTER_N_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"/home/jmin/isaac_ws/isaaclab_test/hamster_n.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 1.05),
+        joint_pos={
+            "LF_HAA": -0.00,
+            "RH_HAA": -0.00,
+            "LH_HAA": 0.00,
+            "RF_HAA": 0.00,
+            "LF_HFE": 0.04,
+            "RH_HFE": 0.04,
+            "LH_HFE": -0.04,
+            "RF_HFE": -0.04,
+            "LF_KFE": -0.13,
+            "RH_KFE": -0.13,
+            "LH_KFE": 0.13,
+            "RF_KFE": 0.13,
+        },
+        joint_vel={".*": 0.4},
+    ),
+    soft_joint_pos_limit_factor=0.40,
+    actuators={
+        "base_legs": IdealPDActuatorCfg(
+            joint_names_expr=[".*HAA", ".*HFE", ".*KFE"],
+            effort_limit=50.0,
+            #saturation_effort=330.0,
+            velocity_limit=21.0,
+            stiffness=20.0,
+            damping=5.0,
+            friction=0.0,
+        ),
+    },
+)
+"""Configuration of Unitree A1 using DC motor."""
+
+
+UNITREE_A1_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/Unitree/A1/a1.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.42),
+        joint_pos={
+            ".*L_hip_joint": 0.1,
+            ".*R_hip_joint": -0.1,
+            "F[L,R]_thigh_joint": 0.8,
+            "R[L,R]_thigh_joint": 1.0,
+            ".*_calf_joint": -1.5,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "base_legs": DCMotorCfg(
+            joint_names_expr=[".*_hip_joint", ".*_thigh_joint", ".*_calf_joint"],
+            effort_limit=33.5,
+            saturation_effort=33.5,
+            velocity_limit=21.0,
+            stiffness=25.0,
+            damping=0.5,
+            friction=0.0,
+        ),
+    },
+)
+"""Configuration of Unitree A1 using DC motor.
+
+Note: Specifications taken from: https://www.trossenrobotics.com/a1-quadruped#specifications
+"""
+
+
+ANYMAL_C_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-C/anymal_c.usd",
+        # usd_path=f"{ISAAC_NUCLEUS_DIR}/Robots/ANYbotics/anymal_instanceable.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.02, rest_offset=0.0),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.6),
+        joint_pos={
+            ".*HAA": 0.0,  # all HAA
+            ".*F_HFE": 0.4,  # both front HFE
+            ".*H_HFE": -0.4,  # both hind HFE
+            ".*F_KFE": -0.8,  # both front KFE
+            ".*H_KFE": 0.8,  # both hind KFE
+        },
+    ),
+    actuators={"legs": ANYDRIVE_3_LSTM_ACTUATOR_CFG},
+    soft_joint_pos_limit_factor=0.95,
+)
+"""Configuration of ANYmal-C robot using actuator-net."""
+
+
+ANYMAL_D_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-D/anymal_d.usd",
+        # usd_path=f"{ISAACLAB_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-D/anymal_d_minimal.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.02, rest_offset=0.0),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.6),
+        joint_pos={
+            ".*HAA": 0.0,  # all HAA
+            ".*F_HFE": 0.4,  # both front HFE
+            ".*H_HFE": -0.4,  # both hind HFE
+            ".*F_KFE": -0.8,  # both front KFE
+            ".*H_KFE": 0.8,  # both hind KFE
+        },
+    ),
+    actuators={"legs": ANYDRIVE_3_LSTM_ACTUATOR_CFG},
+    soft_joint_pos_limit_factor=0.95,
+)
+"""Configuration of ANYmal-D robot using actuator-net.
+
+Note:
+    Since we don't have a publicly available actuator network for ANYmal-D, we use the same network as ANYmal-C.
+    This may impact the sim-to-real transfer performance.
+"""
